291885b0cab0329bab08fb0f9e6ca6a029c04ebc
Gonzalo <gonzalo.castro@robotsandpencils.com>
2016-07-20 09:52:55 -0400

    AL-1298: adds hideInputContainer() method and supporting private NSLayoutConstraint

--- Chatto/Source/ChatController/ChatViewController.swift
+++ Chatto/Source/ChatController/ChatViewController.swift
@@ -110,6 +110,8 @@ public class ChatViewController: UIViewController, UICollectionViewDataSource, U
     }
 
     private var inputContainerBottomConstraint: NSLayoutConstraint!
+    private var topConstraint: NSLayoutConstraint!
+    private var heightConstraint: NSLayoutConstraint!
     private func addInputViews() {
         self.inputContainer = UIView(frame: CGRect.zero)
         self.inputContainer.autoresizingMask = .None
@@ -124,7 +126,9 @@ public class ChatViewController: UIViewController, UICollectionViewDataSource, U
 
         let inputView = self.createChatInputView()
         self.inputContainer.addSubview(inputView)
-        self.inputContainer.addConstraint(NSLayoutConstraint(item: self.inputContainer, attribute: .Top, relatedBy: .Equal, toItem: inputView, attribute: .Top, multiplier: 1, constant: 0))
+        heightConstraint = NSLayoutConstraint(item: self.inputContainer, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 0)
+        topConstraint = NSLayoutConstraint(item: self.inputContainer, attribute: .Top, relatedBy: .Equal, toItem: inputView, attribute: .Top, multiplier: 1, constant: 0)
+        self.inputContainer.addConstraint(topConstraint)
         self.inputContainer.addConstraint(NSLayoutConstraint(item: self.inputContainer, attribute: .Leading, relatedBy: .Equal, toItem: inputView, attribute: .Leading, multiplier: 1, constant: 0))
         self.inputContainer.addConstraint(NSLayoutConstraint(item: self.inputContainer, attribute: .Bottom, relatedBy: .Equal, toItem: inputView, attribute: .Bottom, multiplier: 1, constant: 0))
         self.inputContainer.addConstraint(NSLayoutConstraint(item: self.inputContainer, attribute: .Trailing, relatedBy: .Equal, toItem: inputView, attribute: .Trailing, multiplier: 1, constant: 0))
@@ -151,6 +155,12 @@ public class ChatViewController: UIViewController, UICollectionViewDataSource, U
             self.updateQueue.start()
             self.isFirstLayout = false
         }
+    }
+
+    public func hideInputContainer(hide: Bool) {
+        topConstraint.active = !hide
+        heightConstraint.active = hide
+        self.inputContainer.setNeedsUpdateConstraints()
     }
 
     private func adjustCollectionViewInsets() {
