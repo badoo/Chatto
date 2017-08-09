6dc92db9af3c162c80119c87e91ebb2d22bf8f40
Jürgen Punz <juergen.punz@icloud.com>
2017-08-09 15:58:17 +0200

    Bugfix

--- ChattoAdditions/Source/Chat Items/BaseMessage/Views/BaseMessageCollectionViewCellDefaultStyle.swift
+++ ChattoAdditions/Source/Chat Items/BaseMessage/Views/BaseMessageCollectionViewCellDefaultStyle.swift
@@ -121,7 +121,7 @@ open class BaseMessageCollectionViewCellDefaultStyle: BaseMessageCollectionViewC
     private lazy var dateFont: UIFont = self.dateTextStyle.font()
     private lazy var dateFontColor: UIColor = self.dateTextStyle.color()
 
-    private lazy var dateStringAttributes: [NSAttributedString : AnyObject] = {
+    private lazy var dateStringAttributes: [NSAttributedStringKey : AnyObject] = {
         return [
             NSAttributedStringKey.font: self.dateFont,
             NSAttributedStringKey.foregroundColor: self.dateFontColor
