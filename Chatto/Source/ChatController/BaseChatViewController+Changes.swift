b2f51392a3d34045a61f6350b3b79fb75f8c9fa9
Anton Schukin <anton.schukin@corp.badoo.com>
2017-08-21 16:09:18 +0100

    Fixed issue with accessing UICollectionView width from background thread

--- Chatto/Source/ChatController/BaseChatViewController+Changes.swift
+++ Chatto/Source/ChatController/BaseChatViewController+Changes.swift
@@ -301,7 +301,7 @@ extension BaseChatViewController: ChatDataSourceDelegateProtocol {
             let layoutData = intermediateLayoutData.map { (intermediateLayoutData: IntermediateItemLayoutData) -> ItemLayoutData in
                 return (height: intermediateLayoutData.height!, bottomMargin: intermediateLayoutData.bottomMargin)
             }
-            return ChatCollectionViewLayoutModel.createModel(self.collectionView.bounds.width, itemsLayoutData: layoutData)
+            return ChatCollectionViewLayoutModel.createModel(collectionViewWidth, itemsLayoutData: layoutData)
         }
 
         let isInbackground = !Thread.isMainThread
