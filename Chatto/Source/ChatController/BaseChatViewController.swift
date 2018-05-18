d6b5cbdc2c079f80d5e84a1ebef2cf34c2eb4575
Ruslan Ahapkin <ruslan.ahapkin@corp.badoo.com>
2018-05-18 18:24:31 +0100

    Remove duplicate.

--- Chatto/Source/ChatController/BaseChatViewController.swift
+++ Chatto/Source/ChatController/BaseChatViewController.swift
@@ -64,7 +64,7 @@ open class BaseChatViewController: UIViewController, UICollectionViewDataSource,
  
     // If set to false messages will start appearing on top and goes down
     // If true then messages will start from bottom and goes up.
-    public var placeMessagesFromBottom = false {
+    public var placeMessagesFromBottom = true {
         didSet {
             self.adjustCollectionViewInsets(shouldUpdateContentOffset: false)
         }
