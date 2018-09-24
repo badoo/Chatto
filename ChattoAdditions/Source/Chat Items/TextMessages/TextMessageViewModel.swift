b899207f63c90df249ae5c4da600b7be97aec206
dive <artyom.loenko@mac.com>
2018-09-24 13:58:16 +0100

    added accessibility identifiers for the text message

--- ChattoAdditions/Source/Chat Items/TextMessages/TextMessageViewModel.swift
+++ ChattoAdditions/Source/Chat Items/TextMessages/TextMessageViewModel.swift
@@ -26,6 +26,8 @@ import Foundation
 
 public protocol TextMessageViewModelProtocol: DecoratedMessageViewModelProtocol {
     var text: String { get }
+    var cellAccessibilityIdentifier: String { get }
+    var bubbleAccessibilityIdentifier: String { get }
 }
 
 open class TextMessageViewModel<TextMessageModelT: TextMessageModelProtocol>: TextMessageViewModelProtocol {
@@ -34,6 +36,8 @@ open class TextMessageViewModel<TextMessageModelT: TextMessageModelProtocol>: Te
     }
     public let textMessage: TextMessageModelT
     public let messageViewModel: MessageViewModelProtocol
+    public let cellAccessibilityIdentifier = "chatto.message.text.cell"
+    public let bubbleAccessibilityIdentifier = "chatto.message.text.bubble"
 
     public init(textMessage: TextMessageModelT, messageViewModel: MessageViewModelProtocol) {
         self.textMessage = textMessage
