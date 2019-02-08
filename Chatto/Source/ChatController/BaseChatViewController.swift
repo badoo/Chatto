d50916fadc6ef49a5613c504d2bced5e2b417019
lime <lime@amazeusgpl.com>
2019-02-08 13:50:38 +0300

    Fix crash, if BaseChatViewController destroyed too early

--- Chatto/Source/ChatController/BaseChatViewController.swift
+++ Chatto/Source/ChatController/BaseChatViewController.swift
@@ -146,7 +146,7 @@ open class BaseChatViewController: UIViewController, UICollectionViewDataSource,
 
     open override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        self.keyboardTracker.stopTracking()
+        self.keyboardTracker?.stopTracking()
     }
 
     private func addCollectionView() {
