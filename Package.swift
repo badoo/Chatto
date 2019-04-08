e917767016e83155843044ea860882c6aa6e4e12
Cihat Gündüz <commits@cihatguenduez.de>
2019-04-08 12:21:26 +0200

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "Chatto",
+    // platforms: [.iOS("9.0")],
+    products: [
+        .library(name: "Chatto", targets: ["Chatto"]),
+        .library(name: "ChattoAdditions", targets: ["ChattoAdditions"]),
+    ],
+    targets: [
+        .target(
+            name: "Chatto",
+            path: "Chatto/Source"
+        ),
+        .target(
+            name: "ChattoAdditions",
+            dependencies: ["Chatto"],
+            path: "ChattoAdditions/Source"
+        )
+    ]
+)
