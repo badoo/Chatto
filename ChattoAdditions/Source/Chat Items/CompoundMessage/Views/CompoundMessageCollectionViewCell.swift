724fe96a2fd90e06f12ea72126633a2e4edcf790
Mikhail Gasanov <mikhail.gasanov@corp.badoo.com>
2019-06-27 17:00:50 +0100

    Long gesture recognizer setup in CompoundMessageCollectionViewCell

--- ChattoAdditions/Source/Chat Items/CompoundMessage/Views/CompoundMessageCollectionViewCell.swift
+++ ChattoAdditions/Source/Chat Items/CompoundMessage/Views/CompoundMessageCollectionViewCell.swift
@@ -24,9 +24,39 @@
 @available(iOS 11, *)
 public final class CompoundMessageCollectionViewCell: BaseMessageCollectionViewCell<CompoundBubbleView> {
 
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+
+    private func commonInit() {
+        // 0 would interfere with scrolling
+        self.longPressGestureRecognizer.minimumPressDuration = 0.05
+    }
+
     public override func createBubbleView() -> CompoundBubbleView! {
         return CompoundBubbleView()
     }
 
     public var viewReferences: [ViewReference]?
+
+    // MARK: - UIGestureRecognizerDelegate
+
+    public override func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        // Default implementation is false. Don't allow scroll to be happened during long press gesture on temporary photos
+        return false
+    }
+
+    public override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        let shouldBegin = super.gestureRecognizerShouldBegin(gestureRecognizer)
+        guard gestureRecognizer == self.longPressGestureRecognizer else { return shouldBegin }
+        return shouldBegin && (self.onGestureRecognizerShouldBegin?(gestureRecognizer) ?? false)
+    }
+
+    public var onGestureRecognizerShouldBegin: ((_ gestureRecognizer: UIGestureRecognizer) -> Bool)?
 }
