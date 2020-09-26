18d92d6fb65d16dea8103938c3da23bb480816a0
Victor Shamanov <wiruzx@gmail.com>
2020-09-26 18:27:21 +0100

    Add script for running tests

---
+++ scripts/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+WORKSPACE='Chatto.xcworkspace'
+SCHEME='Chatto'
+
+case "$1" in
+    chatto) ;;
+    additions)
+        SCHEME='ChattoAdditions'
+        ;;
+    app)
+        WORKSPACE='ChattoApp/ChattoApp.xcworkspace'
+        SCHEME='ChattoApp'
+        ;;
+    *) echo "Invalid operation"
+        exit 1
+        ;;
+esac
+
+xcodebuild clean build test \
+    -workspace $WORKSPACE \
+    -scheme $SCHEME \
+    -sdk iphonesimulator \
+    -destination 'platform=iOS Simulator,name=iPhone 11' \
+    -configuration Debug | xcpretty
