230847a2283ed698111e894f029d7b21fee7e1dc
Victor Shamanov <wiruzx@gmail.com>
2022-01-14 15:53:46 +0000

    Add missing UIKit imports

--- ChattoAdditions/sources/Chat Items/Composable/Layout/LayoutProviderProtocol.swift
+++ ChattoAdditions/sources/Chat Items/Composable/Layout/LayoutProviderProtocol.swift
@@ -21,6 +21,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+import UIKit
+
 public protocol LayoutProviderProtocol {
     associatedtype Layout: LayoutModel
     func layout(for context: LayoutContext) throws -> Layout
